ffc29a7defb622f8f80e9d000c49d4d73489ca43
Lucas Davila <nightdavila@gmail.com>
2023-07-27 22:22:54 -0300

    Project created - MEOW

---
+++ SepCatServer/src/main/java/br/com/SepCatServer/Backend/SepCatServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.SepCatServer.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SepCatServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SepCatServerApplication.class, args);
+	}
+
+}
---
+++ SepCatServer/src/test/java/br/com/SepCatServer/Backend/SepCatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.SepCatServer.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SepCatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
